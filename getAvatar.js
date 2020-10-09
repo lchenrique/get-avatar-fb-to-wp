c474a309fcbeeaeafcfe14a28960aaf9ce4ce19b
Lucian <lc.henriquee@gmail.com>
2020-10-09 19:35:48 -0300

    -----

--- getAvatar.js
+++ getAvatar.js
@@ -6,14 +6,23 @@ const Axios = require('axios');
 
 const avatar = async (user, pass) => {
     let browser = await puppeteer.launch({
+        defaultViewport:{width:1400, height:800},
+        timeout:50000,
         args: [
+            '--headless',
             '--no-sandbox',
             '--disable-setuid-sandbox',
-        ], userDataDir: './myUserDataDir',
-        headless: false,
-        defaultViewport: { width: 1400, height: 800, deviceScaleFactor: 1 }
+            '--disable-gpu',
+            '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+            '--user-data-dir=/tmp/user_data/',
+            '--window-size=1920x1080'
+        ],
+        headless: false
     });
+
+
     const page = await browser.newPage();
+
 
     await page.goto('https://www.facebook.com/');
     const isNotloggedin = await page.evaluate(async () => {
@@ -24,21 +33,23 @@ const avatar = async (user, pass) => {
         await page.type('#email', user);
         await page.type('#pass', pass);
         await page.click('#u_0_b');
-        await page.waitForNavigation(); // <------------------------- Wait for Navigation
     }
 
-
+    await page.waitForSelector('[aria-label="Deixe um comentário"]')
     await page.click('[aria-label="Deixe um comentário"]')
-    await page.waitFor(3000)
-    await page.evaluate(async () => {
-        const input = await document.querySelector('._1rwk')
-        const list = await input.querySelector('ul')
-        list.querySelector('[aria-label="Publique uma figurinha de avatar"]')
+    await page.waitForSelector('._1rwk')
+    // await page.evaluate(async () => {
+    //     const input = await document.querySelector('._1rwk')
+    //     console.log(input)
+    //     const list = await input.querySelector('ul')
+    //     list.querySelector('[aria-label="Publique uma figurinha de avatar"]')
 
-    })
+    // })
 
+    await page.waitForSelector('.sp_CaOautnkgPg.sx_546e2d')
     await page.click('.sp_CaOautnkgPg.sx_546e2d')
-    await page.waitFor(3000)
+    
+    await page.waitForSelector('._8rlv')
 
     const imgList = await page.evaluate(() => {
         const avatarContent = document.querySelector('._8rlv')
